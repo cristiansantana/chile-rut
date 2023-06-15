5fe1af4e5a465150b168e63c917ccd157ca8e5bb
Cristian Santana <hola@cristiansantana.cl>
2023-06-14 21:46:44 -0400

    add tests to _getNormalizedRut

--- src/normalizers.test.ts
+++ src/normalizers.test.ts
@@ -1,6 +1,6 @@
 import { _getNormalizedRut } from "./normalizers";
 
-describe("_getNormalizedRut", () => {
+describe("testing _getNormalizedRut function", () => {
 
     test("normalize 11222333-4", () => {
         expect(_getNormalizedRut("11222333-4")).toBe("11222333-4");
@@ -18,6 +18,14 @@ describe("_getNormalizedRut", () => {
         expect(_getNormalizedRut("1-1")).toBe("1-1");
     });
 
+    test("normalize 0.000.001-0", () => {
+        expect(_getNormalizedRut("0.000.001-0")).toBe("1-0");
+    });
+
+    test("normalize 0.000.000-0", () => {
+        expect(() => _getNormalizedRut("0.000.000-0")).toThrow();
+    });
+
     test("normalize \"  11.222.333-4\"", () => {
         expect(() => _getNormalizedRut("  11,222,333-4")).toThrow();
     });
@@ -28,6 +36,10 @@ describe("_getNormalizedRut", () => {
 
     test("normalize \"\"", () => {
         expect(() => _getNormalizedRut("")).toThrow();
+    });
+
+    test("normalize \"1\"", () => {
+        expect(() => _getNormalizedRut("1")).toThrow();
     });
 
     test("normalize \"a\"", () => {
