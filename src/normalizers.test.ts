a2b3bb98814c0c123e2c399bcf5411be0bd04ed6
Cristian Santana <hola@cristiansantana.cl>
2023-06-14 21:35:19 -0400

    add tests to _getNormalizedRut

--- src/normalizers.test.ts
+++ src/normalizers.test.ts
@@ -14,11 +14,27 @@ describe("_getNormalizedRut", () => {
         expect(_getNormalizedRut("11,222,333-4")).toBe("11222333-4");
     });
 
+    test("normalize 1-1", () => {
+        expect(_getNormalizedRut("1-1")).toBe("1-1");
+    });
+
     test("normalize \"  11.222.333-4\"", () => {
         expect(() => _getNormalizedRut("  11,222,333-4")).toThrow();
     });
 
     test("normalize 11x222x333-4", () => {
         expect(() => _getNormalizedRut("11x222x333-4")).toThrow();
+    });
+
+    test("normalize \"\"", () => {
+        expect(() => _getNormalizedRut("")).toThrow();
+    });
+
+    test("normalize \"a\"", () => {
+        expect(() => _getNormalizedRut("a")).toThrow();
+    });
+
+    test("normalize \"0\"", () => {
+        expect(() => _getNormalizedRut("0")).toThrow();
     });
 });
